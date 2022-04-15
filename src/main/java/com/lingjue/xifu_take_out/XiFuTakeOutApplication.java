7e16af50ef017b8dbefc56d48af53213ef2a920c
LingJue <1248290205@qq.com>
2022-04-15 18:28:13 +0800

    Initial commit

---
+++ src/main/java/com/lingjue/xifu_take_out/XiFuTakeOutApplication.java
@@ -0,0 +1,13 @@
+package com.lingjue.xifu_take_out;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiFuTakeOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiFuTakeOutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lingjue/xifu_take_out/XiFuTakeOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingjue.xifu_take_out;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiFuTakeOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
